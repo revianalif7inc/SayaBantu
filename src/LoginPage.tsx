d0aee0f9597164fdafe6f20de0372e79abc2b988
revianalif7inc <revianalif7inc@gmail.com>
2025-12-04 09:44:35 +0700

    fix: correct react-icons import path

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,8 +1,9 @@
 // src/LoginPage.tsx
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom'; // <-- tambah Link
-import { IconContext } from 'react-icons';
-import { HiOutlineMail, HiOutlineLockClosed } from 'react-icons/hi';
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { HiOutlineMail, HiOutlineLockClosed } from "react-icons/hi";
+
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
